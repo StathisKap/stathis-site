b1feb2819eb66f937ffb652ceecfcdb849bb9471
StathisKap <stathiskap75@gmail.com>
2022-10-25 08:50:15 +0000

    Sidebar doesn't overlap now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
 <link rel="stylesheet" href="/src/output.css" />
 
-<div class="flex flex-row">
+<div class="flex min-h-screen">
 	<aside
 		class="
         w-1/5
@@ -15,26 +15,23 @@
         text-center
       text-gray-100
         text-lg
-        fixed 
         inset-y-0
         left-0
         z-10
         "
 	>
-		<div class="flex flex-col">
-			<div class="bg-slate-100">
-				<Logo />
-			</div>
-			{#each pages as page}
-				<div class="border-b-2 border-black">
-					<div class="m-3 leading-8">
-						<a href="/{page.toLowerCase()}">{page}</a>
-					</div>
-				</div>
-			{/each}
+		<div class="bg-slate-100">
+			<Logo />
 		</div>
+		{#each pages as page}
+			<div class="border-b-2 border-black">
+				<div class="m-3 leading-8">
+					<a href="/{page.toLowerCase()}">{page}</a>
+				</div>
+			</div>
+		{/each}
 	</aside>
-	<div class="inline-flex right-0 w-4/5">
+	<div>
 		<slot />
 	</div>
 </div>
