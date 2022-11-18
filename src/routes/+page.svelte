5dd85a0a7827e0a2113c39afb4b9a1b1e865997a
StathisKap <stathiskap75@gmail.com>
2022-11-18 21:55:53 +0000

    Added some text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
 	import Typewriter from 'svelte-typewriter';
 	let is_blinking = false;
 	let is_subtitle_done = false;
-
 </script>
 
 <div in:fade class="text-lg">
@@ -18,7 +17,7 @@
 					is_blinking = true;
 				}}
 			>
-			<td class="font-Menlo">whoami</td>
+				<td class="font-Menlo">whoami</td>
 			</Typewriter>
 			{#if is_blinking}
 				<td class="animate-blink">▐▌</td>
@@ -28,33 +27,48 @@
 </div>
 
 {#if is_blinking}
-<div in:fly="{{ y: -40, duration: 1500 }}">
-	<span class="block font-mono text-center text-8xl lg:text-8xl sm:text-5xl font-extrabold uppercase pt-10">Stathis Kapnidis</span>
-</div>
-<div class="text-center text-xl lg:text-xl sm:text-lg whitespace-nowrap uppercase pt-6 font-Menlo">
-	<span in:fly="{{ y: 40, duration: 1000, delay: 1000}}" class="inline-block"  on:introend={function () { is_subtitle_done = true}}>Software Dev</span>
-	<span in:fly="{{ y: 40, duration: 1000, delay: 1125}}" class="inline-block">&nbsp/&nbsp</span>
-	<span in:fly="{{ y: 40, duration: 1000, delay: 1250}}" class="inline-block">Mechanical Engineering</span>
-	<span in:fly="{{ y: 40, duration: 1000, delay: 1375}}" class="inline-block">&nbsp/&nbsp</span>
-	<span in:fly="{{ y: 40, duration: 1000, delay: 1500}}" class="inline-block">Robotics</span>
-</div>
+	<div in:fly={{ y: -40, duration: 1500 }}>
+		<span
+			class="block font-mono text-center text-8xl lg:text-8xl sm:text-5xl font-extrabold uppercase pt-10"
+			>Stathis Kapnidis</span
+		>
+	</div>
+	<div
+		class="text-center text-xl lg:text-xl sm:text-lg whitespace-nowrap uppercase pt-6 font-Menlo"
+	>
+		<span
+			in:fly={{ y: 40, duration: 1000, delay: 1000 }}
+			class="inline-block"
+			on:introend={function () {
+				is_subtitle_done = true;
+			}}>Software Dev</span
+		>
+		<span in:fly={{ y: 40, duration: 1000, delay: 1125 }} class="inline-block">&nbsp/&nbsp</span>
+		<span in:fly={{ y: 40, duration: 1000, delay: 1250 }} class="inline-block"
+			>Mechanical Engineering</span
+		>
+		<span in:fly={{ y: 40, duration: 1000, delay: 1375 }} class="inline-block">&nbsp/&nbsp</span>
+		<span in:fly={{ y: 40, duration: 1000, delay: 1500 }} class="inline-block">Robotics</span>
+	</div>
 {/if}
 
 {#if is_subtitle_done}
 	<div in:fade class="pt-12 text-lg">
-		&nbsp;
-		&nbsp;
-		&nbsp;
-		&nbsp;
-		Mechanical Engineering with Robotics Graduate from the University of Sussex looking for an Engineering or Software Engineering Job.
- 		Preferably in London.
- 		I have the right to work in the UK under the EUSS.
- 		I have Pre-settled Status, and am currently applying for fully settled.
- 		Fluent in English.
- 		Fast Learner.
- 		Very eager to work.
+		<hr class="mt-12 mb-12" />
+		<p>
+			&nbsp; &nbsp; &nbsp; &nbsp; Mechanical Engineering with Robotics Graduate from the University
+			of Sussex looking for an Engineering or Software Engineering Job. Preferably in London. I have
+			the right to work in the UK under the EUSS. I have Pre-settled Status, and am currently
+			applying for fully settled. Fluent in English. Fast Learner. Very eager to work.
+		</p>
 
-		<hr class="p-12">
-
+		<hr class="mt-12 mb-12" />
+		<p class="text-xl text-slate-500 text-center">
+			Website is a work in progress.
+			It is being developed with Svelte, Typescript, and Tailwind. It is hosted as a node server
+			within an LXC container on the ProxMox Type 1 Hypervisor, running on an old laptop in my house in Greece.
+			The site is accessible through a cloudflare argos tunnel, using the `cloudflared` deamon on a different 
+			LXC container. The domain name is a free one from freenom. So this entire site is 100% from scrath and 100% free. (except power bill ofc)
+		</p>
 	</div>
 {/if}
