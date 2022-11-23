dd32bcfcbf99f238e0e523c55db4501a2ea6103d
StathisKap <stathiskap75@gmail.com>
2022-11-23 03:50:52 +0000

    Added an animation to the text on my name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,8 @@
 {#if is_blinking}
 	<div in:fly={{ y: -40, duration: 1500 }}>
 		<span
-			class="block font-mono text-center text-8xl lg:text-8xl sm:text-5xl font-extrabold uppercase pt-10 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-fuchsia-800 to-indigo-500"
+			class="block font-mono text-center text-8xl lg:text-8xl sm:text-5xl font-extrabold uppercase pt-10 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-fuchsia-800 to-indigo-500 animate-pan"
+			style="background-size:200%;"
 			>Stathis Kapnidis</span>
 	</div>
 	<div
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,13 +7,18 @@ module.exports = {
 	theme: {
 		extend: {
 			animation: {
-				blink: 'blink 1s infinite'
+				blink: 'blink 1s infinite',
+				pan: 'pan 3s linear infinite',
 			},
 			keyframes: {
 				blink: {
 					'0%, 100%': { opacity: 1 },
 					'50%': { opacity: 0 },
-				}
+				},
+				pan: {
+					'from': { 'background-position': '0% center' },
+					'to': { 'background-position': '-200% center' },
+				},
 			},
 			fontFamily: {
 				Menlo: "Menlo",
