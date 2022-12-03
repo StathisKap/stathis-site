c77f1af1d42e5c46a1824958c9bb0d2b28d0ae98
StathisKap <stathiskap75@gmail.com>
2022-12-03 04:08:35 +0000

    Added the cards at the projects page. The radial effect doesn't follow cursor yet

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,12 +1,46 @@
 <script lang="ts">
-    import { fade } from "svelte/transition";
+	import { fade } from 'svelte/transition';
+
+	function handleMouse(e:MouseEvent) {
+		for (const card of document.getElementsByClassName("card") as HTMLCollectionOf<HTMLElement>){
+			const rect = card.getBoundingClientRect(),
+				x = e.clientX - rect.left,
+				y = e.clientY - rect.top;
+			
+			card.style.setProperty("--mouse-x", `$(x)px`);
+			card.style.setProperty("--mouse-y", `$(y)px`);
+		}
+	}
 </script>
 
-<div in:fade={{delay: 100}} >
-    <p class="text-lg"> 
-        This page is being built.
-    </p>
-    <p class="text-lg"> 
-        It will contain showcases of my projects on <a href="https://github.com/StathisKap" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Github</a>
-    </p>
+
+<div
+	in:fade={{ delay: 100 }}
+	class="flex flex-wrap gap-3 max-w-full w-auto justify-center items-center"
+>
+	<div class="h-64 w-80 bg-zinc-900 border-2 border-white border-opacity-10 cursor-none relative card" />
+	<div class="h-64 w-80 bg-zinc-900 border-2 border-white border-opacity-10 cursor-none relative card" />
+	<div class="h-64 w-80 bg-zinc-900 border-2 border-white border-opacity-10 cursor-none relative card" />
+	<div class="h-64 w-80 bg-zinc-900 border-2 border-white border-opacity-10 cursor-none relative card" />
+	<div class="h-64 w-80 bg-zinc-900 border-2 border-white border-opacity-10 cursor-none relative card" />
+	<div class="h-64 w-80 bg-zinc-900 border-2 border-white border-opacity-10 cursor-none relative card" />
 </div>
+
+<style>
+	.card::before {
+		background: radial-gradient(
+/*			800px circle at var(--mouse-x) var(--mouse-y), */
+			800px circle at 100px 100px,
+			rgba(255, 255, 255, 0.1),
+			transparent 40%
+		);
+		height: 100%;
+		width: 100%;
+		left: 0px;
+		top: 0px;
+		position: absolute;
+		content: '';
+		border-radius: inherit;
+		z-index: 2;
+	}
+</style>
