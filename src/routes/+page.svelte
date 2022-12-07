df83cb2efa0e9c188b0ab57c378b267bd6a339ac
StathisKap <stathiskap75@gmail.com>
2022-12-07 08:29:50 +0000

    Added a Download CV button in homepage and Contact page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,6 +89,17 @@
 					</span>
 				</button>
 			</a>
+			<a href="/CV.pdf" download>
+				<button
+					class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium group bg-gradient-to-br from-indigo-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-slate-200 focus:ring-4 focus:outline-none  focus:ring-fuchsia-800 hover:animate-pan"
+				>
+					<span
+						class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 group-hover:bg-opacity-0"
+					>
+						Download CV
+					</span>
+				</button>
+			</a>
 		</div>
 	
 		<hr class="mt-12 mb-12" />
