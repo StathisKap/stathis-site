57f8b5bd421ec918b82b13813ea690da1ac4e3ed
StathisKap <stathiskap75@gmail.com>
2023-03-10 00:48:43 +0000

    Fixed some broken things from the merge

--- src/routes/projects/pathfinder/Node.svelte
+++ src/routes/projects/pathfinder/Node.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
 	import { onMount } from 'svelte';
 
 	enum nodeType {
@@ -18,19 +17,9 @@
 		[nodeType.shortestPath as number]: 'bg-green-500'
 	};
 	export let nodeValue: number = 0;
-=======
-
-	enum nodeType {"unexplored", "start", "end", "explored", "shortestPath"};
-	let state = {	[nodeType.unexplored as number]: "bg-slate-200",
-	 				[nodeType.start as number]: "bg-red-700",
-					[nodeType.end as number]: "bg-yellow-500",
-					[nodeType.explored as number]: "bg-blue-300",
-					[nodeType.shortestPath as number]: "bg-green-500"};
-	export let nodeValue:number = 0;
->>>>>>> main
 
 	let nodeClass: string;
 	$: nodeClass = state[nodeValue];
 </script>
 
-<div class="border-black border-2 w-[20px] h-[20px] {nodeClass}"/>
+<div class="border-black border-2 w-[20px] h-[20px] {nodeClass}" />
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,5 @@
 //import adapter from '@sveltejs/adapter-auto';
-<<<<<<< HEAD
-import adapter from '@sveltejs/adapter-node';
-=======
 import adapter from '@sveltejs/adapter-static';
->>>>>>> main
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
