6baddc8d2811db34d9f0fdaa5788c5f1ff568fc9
StathisKap <stathiskap75@gmail.com>
2023-03-23 12:37:53 +0000

    Fixed the color in the markdown and added some padding

--- src/routes/projects/blasteroids/+page.svelte
+++ src/routes/projects/blasteroids/+page.svelte
@@ -1,8 +1,27 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 	import Loading from '../Loading.svelte';
+	import Markdown from 'svelte-markdown';
+	import { SvelteComponentTyped } from 'svelte';
 
+	let markdownContent: string;
 	let allImagesLoaded = false;
+	let allMarkdownLoaded = false;
+	let allLoaded = false;
+	$: allLoaded = true ? allImagesLoaded && allMarkdownLoaded : false;
+
+	async function loadMarkdown() {
+		const response = await fetch(
+			'https://raw.githubusercontent.com/StathisKap/Blasteroids/main/README.md'
+		);
+		markdownContent = await response.text();
+	}
+
+	onMount(() => {
+		loadMarkdown();
+		allMarkdownLoaded = true
+	});
+
 
 	onMount(() => {
 		const imagesDiv = document.getElementById('Images');
@@ -20,22 +39,32 @@
 	});
 </script>
 
-{#if !allImagesLoaded}
+{#if !allLoaded}
 	<Loading />
 {/if}
 <p
-	class="block text-center text-6xl lg:text-6xl sm:text-5xl font-extrabold uppercase underline from-sky-500 to-yellow-300 text-transparent bg-clip-text bg-gradient-to-r"
+	class="block text-center text-6xl lg:text-6xl sm:text-5xl font-extrabold uppercase underline from-sky-500 to-yellow-300 text-transparent bg-clip-text bg-gradient-to-r pb-10"
 >
 	Blasteroids
 </p>
-<div id="Images" class="flex gap-3 max-w-full w-auto justify-center items-center align-middle">
+<div id="Images" class="flex gap-3 max-w-full w-auto justify-center items-center align-middle pb-7">
 	<img src="/Blasteroids1.png" alt="Blasteroids" class="w-1/2" />
 	<img src="/Blasteroids2.png" alt="Blasteroids" class="w-1/2" />
 </div>
 
-<p>
-	<span class="underline text-lg bold font-Menlo pt-10">How This Works:</span>
+<p id="MD" class="prose max-w-none text-inherit">
+	<Markdown
+		source={markdownContent}
+		options={{
+			gfm: true
+		}}
+	/>
 </p>
-<p>
-	<span> It uses C and allegro 5 to recreate the asteroids game </span>
-</p>
+
+<style>
+	.prose {
+		--tw-prose-headings: rgb(203 213 225);
+		--tw-prose-links: rgb(203 213 225);
+		--tw-prose-code: rgb(203 213 225);
+	}
+</style>
