0afe42977897e6ecbad70d194e6e92678e4ad594
StathisKap <stathiskap75@gmail.com>
2023-03-23 00:08:15 +0000

    added a loading animation to blasteroids page in projects

---
+++ src/routes/projects/Loading.svelte
@@ -0,0 +1,3 @@
+<div class="flex justify-center items-center h-full">
+    <div class="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-purple-500"></div>
+</div>
--- src/routes/projects/blasteroids/+page.svelte
+++ src/routes/projects/blasteroids/+page.svelte
@@ -1,15 +1,41 @@
-<p class="block text-center text-6xl lg:text-6xl sm:text-5xl font-extrabold uppercase underline from-sky-500 to-yellow-300 text-transparent bg-clip-text bg-gradient-to-r">Blasteroids</p>
-<div class="flex gap-3 max-w-full w-auto justify-center items-center align-middle">
-    <img src="/Blasteroids1.png" alt="Blasteroids" class="w-1/2">
-    <img src="/Blasteroids2.png" alt="Blasteroids" class="w-1/2">
+<script>
+	import { onMount } from 'svelte';
+	import Loading from '../Loading.svelte';
+
+	let allImagesLoaded = false;
+
+	onMount(() => {
+		const imagesDiv = document.getElementById('Images');
+		const images = imagesDiv?.querySelectorAll('img');
+		let loadedCount = 0;
+
+		images?.forEach((img) => {
+			img.addEventListener('load', () => {
+				loadedCount++;
+				if (loadedCount === images.length) {
+					allImagesLoaded = true;
+				}
+			});
+		});
+	});
+</script>
+
+{#if !allImagesLoaded}
+	<Loading />
+{/if}
+<p
+	class="block text-center text-6xl lg:text-6xl sm:text-5xl font-extrabold uppercase underline from-sky-500 to-yellow-300 text-transparent bg-clip-text bg-gradient-to-r"
+>
+	Blasteroids
+</p>
+<div id="Images" class="flex gap-3 max-w-full w-auto justify-center items-center align-middle">
+	<img src="/Blasteroids1.png" alt="Blasteroids" class="w-1/2" />
+	<img src="/Blasteroids2.png" alt="Blasteroids" class="w-1/2" />
 </div>
 
 <p>
-    <span class="underline text-lg bold font-Menlo pt-10">How This Works:</span>
-
+	<span class="underline text-lg bold font-Menlo pt-10">How This Works:</span>
 </p>
 <p>
-    <span>
-        It uses C and allegro 5 to recreate the asteroids game
-    </span>
+	<span> It uses C and allegro 5 to recreate the asteroids game </span>
 </p>
