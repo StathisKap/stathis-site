e0d6762c6cffbfdc0d13cb916eca192f8512bfc9
StathisKap <stathiskap75@gmail.com>
2023-03-23 13:47:49 +0000

    Loading animation would displace content instead of overlaying it. Now it's fixed

--- src/routes/projects/Loading.svelte
+++ src/routes/projects/Loading.svelte
@@ -1,3 +1,3 @@
-<div class="flex justify-center items-center h-full">
+<div class="flex justify-center items-center h-screen w-9/12 bg-inherit fixed">
     <div class="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-purple-500"></div>
 </div>
